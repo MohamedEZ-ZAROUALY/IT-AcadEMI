27ef863c0bdff2b76d651267222ae24e22b52742
MohamedEZ-ZAROUALY <ezzaroualymuhammad@gmail.com>
2022-03-07 22:08:08 +0100

    Changes after first deployement

--- backend.php
+++ backend.php
@@ -1,15 +1,19 @@
 <?php
-// the message
-ini_set("SMTP","localhost");
-ini_set("smtp_port","25");
 
-$msg = $_POST["message"];
-$subject = $_POST["msg_subject"];
+$conn = mysqli_connect('localhost:3307','root','','itacademi');
+if(!$conn){
+  echo("<script> alert('Cannot connect to database')</script>");
+}
+else{
+    $first_name = mysqli_real_escape_string ($conn , $_POST["fname"]) ;
+    $last_name = mysqli_real_escape_string ($conn , $_POST["lname"]) ;
+    $phone_number = mysqli_real_escape_string ($conn , $_POST["phone_number"]) ;
+    $email = mysqli_real_escape_string ($conn , $_POST["email"]) ;
+    $genie = mysqli_real_escape_string ($conn , $_POST["genie"]) ;
+    $motivation = mysqli_real_escape_string ($conn , $_POST["message"]) ;
+    
+    $sql = "insert into recruitement values ('$first_name','$last_name','$phone_number','$email','$genie','$motivation')" ;
+    mysqli_query($conn,$sql) ;
+}
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("ezzaroualymuhammad@gmail.com",$subject,$msg);
-header("index.html");
 ?>
